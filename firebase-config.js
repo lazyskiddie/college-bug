bf72ee888d9d537f019e2018b1c06607b281a98c
Sandeep Singh <sandeepsingh08550@gmail.com>
2025-06-03 11:16:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBskRtOihjEZNK3lAfBkO_6t8aTLa5rM9s",
+    authDomain: "college-bug.firebaseapp.com",
+    projectId: "college-bug",
+    storageBucket: "college-bug.appspot.com",
+    messagingSenderId: "794442431593",
+    appId: "1:794442431593:web:fdec0ff65bf94cb2ebf6f8",
+    measurementId: "G-GXGVN65G3R"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
